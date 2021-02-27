607c3e2fb30f7ed1ed13f47e70bf197617fa4e18
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-27 22:46:53 +0100

    The code is ready

---
+++ Day01_Problem01.py
@@ -0,0 +1,8 @@
+# Day01_Problem01:
+# A single line of text denoting x (the variable whose contents must be printed)
+# Print Hello, World. on the first line, and the contents of x  on the second line.
+# The code starts here:
+
+x = str (input())
+print ("Hello, World!")
+print(x)
